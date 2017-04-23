ad7aa16e9f564b42d2d1bf136ca931e677933e8d
wellitried <i1g9o9r4HL>
2017-04-23 03:07:49 +0300

    multiple changes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,16 @@
-var app = angular.module('app', []);
-app.controller('ctrl', function ($scope, $http) {
-    $http.get('/json/0').success(function (data) {
-        $scope.json = data;
-    });
+var app = angular.module('app', ["ngTable"]);
+app.controller('ctrl', function ($scope, $http, NgTableParams) {
+
+    $scope.actualJson = getJson(0);
+    $scope.tableIsShown = function () {
+        return $scope.actualJson ? true : false;
+    };
+
+    $scope.tableParams = new NgTableParams({}, {dataset: $scope.actualJson});
+
+    function getJson(index) {
+        $http.get('/json/' + index).success(function (data) {
+            $scope.actualJson = data;
+        });
+    }
 });
